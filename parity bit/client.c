cad47dfaee8a1c9d7e5c652e2082ddce8241d489
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2025-01-19 10:40:55 +0530

    Update client.c

--- parity bit/client.c
+++ parity bit/client.c
@@ -1,1 +1,34 @@
-//client side code for partiy bit
+//client side code for partiy bit// generaion of data word
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("Enter data\n");
+    gets(str);
+    int count = 0, choice;
+    for(int i=0;i<strlen(str);i++)
+    {
+        if(str[i]=='1')
+            count++;
+    }
+    printf("The no of 1s is %d \n",count);
+    char e_dw[strlen(str)+1];
+    char o_dw[strlen(str)+1];
+    strcpy(e_dw,str);
+     strcpy(o_dw,str);
+    if (count%2==0)
+               { e_dw[strlen(e_dw)] = '0';
+                o_dw[strlen(o_dw)] = '1';
+               }
+    else
+                {
+                e_dw[strlen(e_dw)] = '1';
+                o_dw[strlen(o_dw)] = '0';
+            
+    }
+    printf("Generated even parity : %s\n",e_dw);
+    printf("Generated odd parity : %s",o_dw);
+    
+    
+}
